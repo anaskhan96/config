0587052f1f9ae393c50add3b855c7975cb92c922
Anas Khan <anas.khan96@outlook.com>
2018-03-05 00:40:41 +0530

    added config.fish

---
+++ mac/dotfiles/config.fish
@@ -0,0 +1,35 @@
+fish_vi_key_bindings
+
+alias gs="git stage ."
+alias gc="git commit -m"
+alias push="git push"
+alias v="nvim"
+
+function ip
+	ifconfig | grep "inet.*broadcast"
+end
+
+function wifi
+	networksetup -setairportpower en1 on
+	networksetup -setairportnetwork en1 $argv
+end
+
+function bluon
+	blueutil -p 1
+	echo "Bluetooth switched on"
+end
+
+function bluoff
+	blueutil -p 0
+	echo "Bluetooth switched off"
+end
+
+function sshon
+	sudo systemsetup -setremotelogin on
+	echo "ssh on for user $USER"
+end
+
+function sshoff
+	sudo systemsetup -setremotelogin off
+	echo "ssh off for user $USER"
+end
