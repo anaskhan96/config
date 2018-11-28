9e3bc1e9b7a4cee3ee1eda22593772f505f8e243
Anas Khan <anas.khan96@outlook.com>
2018-11-28 16:34:45 +0530

    typo fix

--- mac/restore_script.sh
+++ mac/restore_script.sh
@@ -63,7 +63,7 @@ echo "= = = zookeeper = = ="
 brew install zookeeper
 echo "= = = nginx = = ="
 brew install nginx
-ehco "= = = noti = = ="
+echo "= = = noti = = ="
 brew install noti
 
 echo "Installing the following packages (cask-specific) :-"
