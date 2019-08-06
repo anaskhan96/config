f848327f070b24eb8078c0d3026ed518351fd5af
Anas Khan <anas.khan96@outlook.com>
2019-08-06 23:35:19 +0530

    updating config to include bash profile

--- mac/dotfiles/config.fish
+++ mac/dotfiles/config.fish
@@ -1,11 +1,38 @@
 fish_vi_key_bindings
 
+# Fish shell
+
+egrep "^export " ~/.bash_profile | while read e
+	set var (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\1/")
+	set value (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\2/")
+
+	# remove surrounding quotes if existing
+	set value (echo $value | sed -E "s/^\"(.*)\"\$/\1/")
+
+	if test $var = "PATH"
+		# replace ":" by spaces. this is how PATH looks for Fish
+		set value (echo $value | sed -E "s/:/ /g")
+
+		# use eval because we need to expand the value
+		eval set -xg $var $value
+
+		continue
+	end
+
+	# evaluate variables. we can use eval because we most likely just used "$var"
+	set value (eval echo $value)
+
+	#echo "set -xg '$var' '$value' (via '$e')"
+	set -xg $var $value
+end
+
 alias gs="git stage ."
 alias gc="git commit -m"
 alias push="git push origin master"
 alias commitC="git rev-list --count --all"
 alias pull="git pull --rebase"
-alias v="nvim"
+alias lg="lazygit"
+alias v="vim"
 export GOPATH="/Users/$USER/go"
 export GOBIN="/Users/$USER/go/bin"
 set PATH $GOBIN $PATH
