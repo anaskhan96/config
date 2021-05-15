9b32139e0093a3b5f142eae85c58b94386d4deda
Anas Khan <anas.khan96@outlook.com>
2021-05-15 19:12:41 +0530

    nitpick

--- mac/misc.bash
+++ mac/misc.bash
@@ -1,5 +1,8 @@
 # install oh my fish and set theme
 
+echo "Installing Oh My Fish and setting the theme"
 curl -L https://get.oh-my.fish | fish
 omf install clearance
 omf theme clearance
+
+echo "VSCode settings synced w Github"
